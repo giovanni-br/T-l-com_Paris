f04258e82455b216215375396dce44ec8cae1a73
Giovanni Benedetti <giovannibenedettidarosa@gmail.com>
2023-12-25 19:24:54 +0100

    Add IMAGE PROCESSING directory

--- IMAGE PROCESSING/IMA201/TP2/import cv2.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-
-path = r'C:\Users\giova\OneDrive\Área de Trabalho\IMA\TP2\femme.tif'
-
-img = cv2.imread(path) 
-
-print(type(img))
